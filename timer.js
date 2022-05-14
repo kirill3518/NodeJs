a81e298854be4d15058af5928db903b9ef13460c
kirill3518 <kirill3518@mail.ru>
2022-05-15 00:41:11 +0500

    Реализован таймер интервала между датами

--- timer.js
+++ timer.js
@@ -2,37 +2,30 @@ import EventEmitter from "events";
 import colors from "colors";
 
 const ERR_INVALID_ARGUMENT = "Неверный формат аргумента";
+const ERR_INVALID_DATE = "Invalid Date";
 
 class MyEmitter extends EventEmitter { };
 const emitterObject = new MyEmitter();
 
+
 // Валидировать формат аргумента
 function ValidateArgument(value) {
-    const result = /^\d{1,2}-\d{1,2}-\d{1,2}-\d{1,2}$/.test(value);
+    const result = /^\d{1,2}-\d{1,2}-\d{1,2}-\d{1,4}$/.test(value);
     if (!result) {
         return ERR_INVALID_ARGUMENT;
+    }
+    if (getDateFromIntervals(value) == ERR_INVALID_DATE) {
+        return ERR_INVALID_DATE;
     }
 
     return null;
 }
 
-// Получить секунды из входной строки "час-день-месяц-год"
-function getSecondsFromIntervals(value) {
-    const intervals = value.split("-");
-    console.log(intervals);
-    let sec = 0;
-    for (let i = 0; i < intervals.length; i++) {
-        if (i === 0) {
-            sec = +intervals[i] * 60 * 60;
-        } else if (i === 1) {
-            sec = sec + +intervals[i] * 24 * 60 * 60;
-        } else if (i === 2) {
-            sec = sec + +intervals[i] * 30 * 24 * 60 * 60;
-        } else if (i === 3) {
-            sec = sec + +intervals[i] * 12 * 30 * 24 * 60 * 60;
-        }
-    }
-    return sec;
+// Получить дату из строки с шаблоном "час-день-месяц-год"
+function getDateFromIntervals(value) {
+    const arr = value.split("-");
+    // return new Date(arr[3], arr[2], arr[1], arr[0]);
+    return new Date(`${arr[3]}-${arr[2]}-${arr[1]}T${arr[0]}:00`);
 }
 
 // Добавить лидирующий ноль в выходную строку
@@ -65,16 +58,19 @@ function getIntervalFromSeconds(value) {
 
 // Установить таймер
 const SetTimer = async (value) => {
-    const timeout = 1;
+    const timeout = 1; // Посекундный вывод
     let promise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (value.seconds > 0) {
-                console.log(`${value.name}: ${getIntervalFromSeconds(value.seconds)}`);
-                const value2 = {
+            const sec = value.date - Date.now();
+            if (sec > 0) {
+                // Вывод в терминал состояния таймеров (сколько осталось)
+                console.log(`${value.name}: ${getIntervalFromSeconds(Math.trunc(sec / 1000))}`);
+                const value3 = {
                     name: value.name,
-                    seconds: value.seconds - timeout
+                    date: value.date
                 };
-                emitterObject.emit(value.name, value2);
+                // Следующий запуск обработчика
+                emitterObject.emit(value.name, value3);
             } else {
                 console.log(colors.green(`${value.name}: Таймер завершил работу`));
             }
@@ -84,11 +80,11 @@ const SetTimer = async (value) => {
 }
 
 /**
- * Алгоритм запускает таймеры, которые отсчитывают заданные интервалы в секундах на убывание до 0.
- * Шаблон аргумента: 99-99-99-99, представляет набор соответствующих интервалов как: часы-дни-месяцы-годы.
+ * Алгоритм запускает таймеры, которые отсчитывают текущий интервал между двумя датами: датой аргумента и текущей датой.
+ * Шаблон аргумента: 23-30-12-9999, представляет набор соответствующих интервалов как: часы-дни-месяцы-годы.
  * Допустимо задавать неограниченное количество аргументов.
  * 
- * Пример запуска скрипта: node timer.js 11-11-11-11 22-22-22-22 23-23-23-23
+ * Пример запуска скрипта: node timer.js 24-07-11-2023 01-15-05-2032
  */
 
 // Получить аргументы из потока ввода
@@ -99,19 +95,19 @@ for (let i = 0; i < args.length; i++) {
     // Валидация аргументов
     const result = ValidateArgument(args[i]);
     if (result !== null) {
-        console.log(colors.red(`Аргумент №${i + 1} "${args[i]}": ${ERR_INVALID_ARGUMENT}`));
+        console.log(colors.red(`Аргумент №${i + 1} "${args[i]}": ${result}`));
         process.exit(1);
     }
 
-    // Обьявить событие
+    //  Создать для каждого аргумента событие (таймер)
     const timerName = `timer_${i}`;
     emitterObject.on(timerName, SetTimer);
 
-    // Установка таймеров
-    let sec = getSecondsFromIntervals(args[i]);
+    // Подготовить данные для обработчика
+    let myDate = getDateFromIntervals(args[i]);
     const value = {
         name: timerName,
-        seconds: sec
+        date: myDate
     };
 
     // Первый запуск обработчика
